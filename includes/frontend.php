58e5dbbe037e8d5773bd96c997b45ce2b3ea83bc
Ryan Benhase <rbenhase@2060digital.com>
2016-07-26 09:16:43 -0400

    Run output through Wordpress' the_content filter

--- includes/frontend.php
+++ includes/frontend.php
@@ -1,12 +1,12 @@
 <div class="fl-rich-text fl-two-column-text">
   <div class="fl-rich-text-col-a">
     <div class="column-inner">
-      <?php echo $settings->text_col_a; ?>
+      <?php echo wpautop( apply_filters( 'the_content', $settings->text_col_a ) ); ?>
     </div>
   </div>
   <div class="fl-rich-text-col-b">
     <div class="column-inner">
-      <?php echo $settings->text_col_b; ?>
+      <?php echo wpautop( apply_filters( 'the_content', $settings->text_col_b ) ); ?>
     </div>
   </div>
 </div>
